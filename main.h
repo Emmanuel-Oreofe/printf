41c20889b1d4570e7d769b01acf371c18a8fb948
Emmanuel-Oreofe <olaleyeemmanueloreofe@gmail.com>
2022-05-25 03:34:05 -0700

    printf

--- main.h
+++ main.h
@@ -36,16 +36,16 @@
 
 typedef struct parameters
 {
-        unsigned int unsign: 1;
-        unsigned int plus_flag: 1;
-        unsigned int space_flag: 1;
-        unsigned int hashtag_flag: 1;
-        unsigned int zero_flag: 1;
-        unsigned int minus_flag: 1;
-        unsigned int width;
-        unsigned int precision;
-        unsigned int h_modifier: 1;
-        unsigned int l_modifier: 1;
+	unsigned int unsign: 1;
+	unsigned int plus_flag: 1;
+	unsigned int space_flag: 1;
+	unsigned int hashtag_flag: 1;
+	unsigned int zero_flag: 1;
+	unsigned int minus_flag: 1;
+	unsigned int width;
+	unsigned int precision;
+	unsigned int h_modifier: 1;
+	unsigned int l_modifier: 1;
 } params_t;
 
 /**
@@ -56,8 +56,8 @@ typedef struct parameters
 
 typedef struct specifier
 {
-        char *specifier;
-        int (*f)(va_list, params_t *);
+	char *specifier;
+	int (*f)(va_list, params_t *);
 } specifier_t;
 
 /* _put.c module */
